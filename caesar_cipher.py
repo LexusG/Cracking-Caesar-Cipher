3605d50542f3462f00a080068c7938a4a14a4ec8
LexusG <33048777+LexusG@users.noreply.github.com>
2023-10-29 07:37:29 -0500

    Update to Code

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,7 @@
-def caesar_cipher(text, shift, decrypt=False):
+def caesar_cipher(text, shift):
     result = ''
     for char in text:
         if char:
-            # Determine whether to encrypt or decrypt
-            if decrypt:
-                shift = -shift
             # Apply the shift
             shifted_char = chr(((ord(char) - ord('a' if char.islower() else 'A') + shift) % 26) + ord('a' if char.islower() else 'A'))
             result += shifted_char
@@ -14,21 +11,18 @@ def caesar_cipher(text, shift, decrypt=False):
 
 def main():
     while True:
-        choice = input("Choose an option (E)ncrypt or (D)ecrypt (Q to quit): ").upper()
+        choice = input("Press (E) to Encrypt or (Q to quit): ").upper()
         
         if choice == 'Q':
             break
-        elif choice in ['E', 'D']:
+        elif choice in ['E']:
             text = input("Enter the message: ")
             shift = int(input("Enter the shift value: "))
             if choice == 'E':
                 encrypted_text = caesar_cipher(text, shift)
                 print("Encrypted message:", encrypted_text)
-            else:
-                decrypted_text = caesar_cipher(text, shift, decrypt=True)
-                print("Decrypted message:", decrypted_text)
         else:
-            print("Invalid choice. Please enter 'E' for Encrypt, 'D' for Decrypt, or 'Q' to quit.")
+            print("Invalid choice. Please enter 'E' for Encrypt, or 'Q' to quit.")
 
 if __name__ == "__main__":
     main()
