d2896c57a5c96a2af1a533d57da5f8a5fe2a8dfa
LexusG <33048777+LexusG@users.noreply.github.com>
2023-10-29 16:52:26 -0500

    Update to Code

--- Caesar's_Cypher_BruteForce_Cracking.py
+++ Caesar's_Cypher_BruteForce_Cracking.py
@@ -1,3 +1,25 @@
  # Caesar's Cypher BruteForce Cracking Script 
 
+decrtpt_message = input("Input Cypher Here: ").upper()
+SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
+# Loop every key 
+for key in range(len(SYMBOLS))
+converted = ""
+
+    #loop through all message in decrtpt_message
+    for symbol in decrtpt_message:
+        if symbol in SYMBOLS:=
+            symbolIndex = SYMBOLS.find(symbol)
+            convertedIndex = symbolIndex - key
+
+            #Handle the SYMBOLS Wraparound:
+            if convertedIndex < 0:
+                convertedIndex = convertedIndex + len(SYMBOLS)
+
+            #Append the decrypted symbol:
+            converted = converted + SYMBOLS[convertedIndex]
+        
+        else:
+            #Append the symbol without encryption/decrypting 
+            converted = converted + symbol 
