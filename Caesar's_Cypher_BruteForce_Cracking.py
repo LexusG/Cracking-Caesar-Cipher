d1d0bb5f0fb85bbedfa260a6411f26f480bd7111
LexusG <33048777+LexusG@users.noreply.github.com>
2023-10-30 00:50:01 -0500

    Update to Code

--- Caesar's_Cypher_BruteForce_Cracking.py
+++ Caesar's_Cypher_BruteForce_Cracking.py
@@ -4,12 +4,12 @@ decrtpt_message = input("Input Cypher Here: ").upper()
 SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
 # Loop every key 
-for key in range(len(SYMBOLS))
-converted = ""
+for key in range(len(SYMBOLS)):
+    converted = ""
 
     #loop through all message in decrtpt_message
     for symbol in decrtpt_message:
-        if symbol in SYMBOLS:=
+        if symbol in SYMBOLS:
             symbolIndex = SYMBOLS.find(symbol)
             convertedIndex = symbolIndex - key
 
@@ -23,3 +23,6 @@ converted = ""
         else:
             #Append the symbol without encryption/decrypting 
             converted = converted + symbol 
+
+    #Display Every Possible decryption:
+    print('Key #%s:  %s' % (key, converted))
